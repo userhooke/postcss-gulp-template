8d90d3e22202af9e358cd325ba49509b9e098586
userhooke <vpelet@gmail.com>
2017-01-26 12:01:26 +0200

    autoprefixer added

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+
+var processors = [
+autoprefixer
+];
+
+
+
+gulp.task('css', function () {
+  return gulp.src('src/css/**/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('dest/css'));
+});
+
+//watch task
+gulp.task('watch', function () {
+   gulp.watch('src/css/**/*.css', ['css']);
+});
+
+//default task
+gulp.task('default', ['css']);
